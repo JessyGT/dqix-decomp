4cee33b89530878d352661654885bdd2279dcdfe
Jessy Grenon-Tremblay (ETS) <jessy.grenon.t@gmail.com>
2026-09-06 17:23:08 -0400

    renamed function to verbose SetBattleStructField0

--- src/Combat/Main/SetBattleStructField0.cpp
+++ src/Combat/Main/SetBattleStructField0.cpp
@@ -1,7 +1,7 @@
 #include <globaldefs.h>
 #include "Combat/Main/BattleList.h"
 
-extern "C" ARM void func_0200fb84(BattleStruct* battleStruct, int value)
+extern "C" ARM void SetBattleStructField0(BattleStruct* battleStruct, int field0Value)
 {
-    battleStruct->unk0 = value;
+    battleStruct->unk0 = field0Value;
 }
